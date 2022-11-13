983bd3a365951f4996bb228dbf13cd7d821511a9
chaeinP <pca0046@gmail.com>
2022-11-13 22:18:29 +0900

    project init

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import cors from 'cors';
+const app: express.Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => res.send('Hello World'));
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import app from './app';
+
+app.listen(8080, () =>
+  console.log(
+    `======================\n🚀 App listening on the port 8080\n======================`,
+  ),
+);
